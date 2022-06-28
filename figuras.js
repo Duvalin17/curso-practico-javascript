0588555924d148355c77adab628c2e4bdac94e03
Emmanuel <duldu35@gmail.com>
2022-06-27 22:13:49 -0500

    triangulo

--- figuras.js
+++ figuras.js
@@ -1,1 +1,18 @@
-console.log("Hello Word");
+console.group("cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden" + ladoCuadrado);
+ 
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimeto del cuadrado es :" + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado *ladoCuadrado ;
+console.log("El area al cuadrado es de  :" + areaCuadrado);
+console.groupEnd();
+/// codigo de triangulo
+console.group("triaungulo");
+const ladoTriangulo1=6;
+const ladoTriangulo2=6;
+const ladoTriangulo3=4;
+console.log("Los lados del triaungulo miuden  :" + ladoTriangulo1 + 
+            "cm , " + ladoTriangulo2 + "cm , "+ ladoTriangulo3 + "cm , ");
+console.groupEnd();            
